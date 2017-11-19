4f9e4792c8e0411f741934a2b90b3fa5b8f80be0
Brett Creeley <bcreeley@pdx.edu>
2017-11-19 01:19:18 -0800

    Give Inmoov a left and right Forearm!
    Todo:  The Forearm object should probably be moved
           inside of the Arm object when it gets
           created.
    
    Signed-off-by: Brett Creeley <bcreeley@pdx.edu>

--- Inmoov.py
+++ Inmoov.py
@@ -9,15 +9,29 @@ Authors:
 '''
 from Head import Head
 from Torso import Torso
+from Forearm import Forearm
 
 class Inmoov(object):
     '''
     This class are Inmoov!
     '''
-    HEAD_X_CHANNEL = 0
-    HEAD_Y_CHANNEL = 1
+    HEAD_X_CHANNEL  = 0
+    HEAD_Y_CHANNEL  = 1
     TORSO_L_CHANNEL = 2
     TORSO_R_CHANNEL = 3
+    L_WRIST_CHANNEL = 4
+    R_WRIST_CHANNEL = 5
+    L_PINKY_FINGER_CHANNEL = 6
+    L_RING_FINGER_CHANNEL  = 7
+    L_MID_FINGER_CHANNEL   = 8
+    L_INDEX_FINGER_CHANNEL = 9
+    L_THUMB_CHANNEL        = 10
+    R_PINKY_FINGER_CHANNEL = 11
+    R_RING_FINGER_CHANNEL  = 12
+    R_MID_FINGER_CHANNEL   = 13
+    R_INDEX_FINGER_CHANNEL = 14
+    R_THUMB_CHANNEL        = 15
+
 
     def __init__(self):
         '''
@@ -25,6 +39,21 @@ class Inmoov(object):
         '''
         self.head = Head(self.HEAD_X_CHANNEL, self.HEAD_Y_CHANNEL)
         self.torso = Torso(self.TORSO_L_CHANNEL, self.TORSO_R_CHANNEL)
+        '''
+        Todo: Move the Forearm objects into the Arm class (unimplemented)
+        '''
+        self.left_forearm = Forearm(self.L_PINKY_FINGER_CHANNEL,
+                                    self.L_RING_FINGER_CHANNEL,
+                                    self.L_MID_FINGER_CHANNEL,
+                                    self.L_INDEX_FINGER_CHANNEL,
+                                    self.L_THUMB_CHANNEL,
+                                    self.L_WRIST_CHANNEL)
+        self.right_forearm = Forearm(self.R_PINKY_FINGER_CHANNEL,
+                                     self.R_RING_FINGER_CHANNEL,
+                                     self.R_MID_FINGER_CHANNEL,
+                                     self.R_INDEX_FINGER_CHANNEL,
+                                     self.R_THUMB_CHANNEL,
+                                     self.R_WRIST_CHANNEL)
 
     def move_head(self, x_degrees=None, y_degrees=None):
         '''
