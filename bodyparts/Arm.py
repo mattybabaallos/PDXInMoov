ef7fc624a40f44ad0bb009ef1ece2014ad10afca
matty <matty.babaallos@gmail.com>
2018-03-23 22:39:05 -0700

    created folders

---
+++ bodyparts/Arm.py
@@ -0,0 +1,32 @@
+#!/bin/python
+"""
+This module contains an Inmoov Arm
+
+Authors:
+    Brett Creeley
+    Matty Baba Allos
+"""
+
+
+class Arm(object):
+    """
+    This class represents an Inmoov Arm
+    """
+
+    def __init__(self, forearm, shoulder):
+        """
+        Build an Inmoov Arm
+        """
+        if forearm is None or shoulder is None:
+            raise "Could not build a arm"
+        self.forearm = forearm
+        self.shoulder = shoulder
+
+
+    #def up(self)
+
+    #def down(self)
+
+    def off(self):
+        self.forearm.off()
+        self.shoulder.off()
